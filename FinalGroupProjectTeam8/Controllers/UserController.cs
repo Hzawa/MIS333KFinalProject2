bc9d6d9bd6c6423c24a5eee1bf5d8613684a0012
Hanzawa <patrick@patrickm.co>
2017-04-30 17:08:28 -0600

    More work for users.

--- FinalGroupProjectTeam8/Controllers/UserController.cs
+++ FinalGroupProjectTeam8/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using FinalGroupProjectTeam8.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,15 +26,18 @@ namespace FinalGroupProjectTeam8.Controllers
                 // Otherwise, stay here
 
                 // Let's get a list of accounts to add to ViewBag
-                var customerId = 1;
+                var userId = System.Web.HttpContext.Current.User.Identity.GetUserId();
                 var accounts = from a in db.BankAccounts
-                               where a.CustomerID.Equals(customerId)
+                               where a.UserID.Equals(userId)
                                select a;
-                ViewBag.Accounts = accounts;
 
                 // Finally, return the view
-                return View();
+                return View(accounts.ToList());
             }
+        }
+
+        public ActionResult ApplyForAccount() {
+            return RedirectToAction("Apply", "BankAccount");
         }
     }
 }
