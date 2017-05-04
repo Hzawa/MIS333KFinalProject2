cd9d8e421f331e6a4e466b139d5ef0e00fed8aa2
kevbot718 <kstevens7@utexas.edu>
2017-05-04 00:27:15 -0500

    Email

---
+++ FinalGroupProjectTeam8/Migrations/201705040512529_mostRecent5.cs
@@ -0,0 +1,26 @@
+namespace FinalGroupProjectTeam8.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mostRecent5 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Transactions", "ReceivingBankAccountID", "dbo.BankAccounts");
+            DropIndex("dbo.Transactions", new[] { "ReceivingBankAccountID" });
+            DropColumn("dbo.Transactions", "ReceivingBankAccountID");
+            DropColumn("dbo.Disputes", "DisputeType");
+            DropColumn("dbo.Disputes", "CorrentAmount");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Disputes", "CorrentAmount", c => c.Int(nullable: false));
+            AddColumn("dbo.Disputes", "DisputeType", c => c.Int(nullable: false));
+            AddColumn("dbo.Transactions", "ReceivingBankAccountID", c => c.String(maxLength: 128));
+            CreateIndex("dbo.Transactions", "ReceivingBankAccountID");
+            AddForeignKey("dbo.Transactions", "ReceivingBankAccountID", "dbo.BankAccounts", "BankAccountID");
+        }
+    }
+}
